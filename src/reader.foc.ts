4cd08aec44d0dcd766f93b128253cac299aafd6d
FOCKUSTY <wwddwwwd@mail.ru>
2024-11-15 01:24:59 +0500

    --style

--- src/reader.foc.ts
+++ src/reader.foc.ts
@@ -1,67 +1,68 @@
-import Logger from 'fock-logger';
+import Logger from "fock-logger";
 
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 
-import Validator from './validator.foc';
-import config from '../config.json';
+import Validator from "./validator.foc";
+import config from "../config.json";
 
 class Reader {
-    private readonly logger = new Logger("Reader");
-    private readonly _path: string = path.join(config.path || "./");
-    private readonly validator = new Validator();
+	private readonly logger = new Logger("Reader");
+	private readonly _path: string = path.join(config.path || "./");
+	private readonly validator = new Validator();
 
-    public constructor() {
-        this.init();
-    };
+	public constructor() {
+		this.init();
+	}
 
-    private readonly isFolder = (p: string): boolean => {
-        const parsed = path.parse(path.join(p));
+	private readonly isFolder = (p: string): boolean => {
+		const parsed = path.parse(path.join(p));
 
-        return parsed.ext === "";
-    };
+		return parsed.ext === "";
+	};
 
-    private readonly ReadFile = (p: string) => {
-        const isFolder = this.isFolder(p);
+	private readonly ReadFile = (p: string) => {
+		const isFolder = this.isFolder(p);
 
-        if (isFolder) this.ReadFolder(p);
-        else {
-            const file = fs.readFileSync(path.join(p), "utf-8");
-            const parsed = path.parse(path.join(p));
-            const name = parsed.name + parsed.ext;
+		if (isFolder) this.ReadFolder(p);
+		else {
+			const file = fs.readFileSync(path.join(p), "utf-8");
+			const parsed = path.parse(path.join(p));
+			const name = parsed.name + parsed.ext;
 
-            this.validator.logger.execute("Валидация...");
+			this.validator.logger.execute("Валидация...");
 
-            const valid = this.validator.init(file);
+			const valid = this.validator.init(file);
 
-            if (!valid)
-                throw new Error(`Файл ${name} не валиден`);
-            else this.validator.logger.execute(`Файл ${name} успешно прошёл валидацию`);
-        };
-    };
+			if (!valid) throw new Error(`Файл ${name} не валиден`);
+			else this.validator.logger.execute(`Файл ${name} успешно прошёл валидацию`);
+		}
+	};
 
-    private readonly ReadFolder = (p: string) => {
-        const files = fs.readdirSync(path.join(p));
+	private readonly ReadFolder = (p: string) => {
+		const files = fs.readdirSync(path.join(p));
 
-        this.logger.execute("Чтение папки " + path.parse(path.join(p)).name);
+		this.logger.execute("Чтение папки " + path.parse(path.join(p)).name);
 
-        for(const file of files) {
-            const parsed = path.parse(path.join(p, file));
-            const fileName = parsed.name + (parsed.ext === "" ? "" : parsed.ext);
+		for (const file of files) {
+			const parsed = path.parse(path.join(p, file));
+			const fileName = parsed.name + (parsed.ext === "" ? "" : parsed.ext);
 
-            this.logger.execute(`Чтение ${parsed.ext === "" ? "папки" : "файла"} ${fileName}`);
+			this.logger.execute(
+				`Чтение ${parsed.ext === "" ? "папки" : "файла"} ${fileName}`
+			);
 
-            this.ReadFile(path.join(p, file));
-        };
-    };
+			this.ReadFile(path.join(p, file));
+		}
+	};
 
-    private init() {
-        this.ReadFolder(this._path);
-    };
-};
+	private init() {
+		this.ReadFolder(this._path);
+	}
+}
 
 (() => {
-    new Reader();
+	new Reader();
 })();
 
 export default Reader;
--- src/validator.foc.ts
+++ src/validator.foc.ts
@@ -1,80 +1,84 @@
 import Logger, { Colors } from "fock-logger";
 
 class Validator {
-    private readonly _file: string | undefined;
-    private readonly _logger = new Logger("Validator");
-    
-    public constructor(file?: string) {
-        this._file = file;
-    };
+	private readonly _file: string | undefined;
+	private readonly _logger = new Logger("Validator");
 
-    private readonly data = (file?: string): [string, string[], boolean] => {
-        if (file)
-            return [file, file.split("\r\n"), true];
+	public constructor(file?: string) {
+		this._file = file;
+	}
 
-        if (this._file)
-            return [this._file, this._file.split("\r\n"), true];
+	private readonly data = (file?: string): [string, string[], boolean] => {
+		if (file) return [file, file.split("\r\n"), true];
 
-        const fl = file || this._file;
+		if (this._file) return [this._file, this._file.split("\r\n"), true];
 
-        if (!fl)
-            return ["", [], false];
+		const fl = file || this._file;
 
-        const lines = fl.split("\r\n");
+		if (!fl) return ["", [], false];
 
-        return [ fl, lines, true ];
-    };
+		const lines = fl.split("\r\n");
 
-    private readonly PrintErrorFix = (text: string, error: string, file: string) => {
-        const start = file.indexOf(`${text}`);
+		return [fl, lines, true];
+	};
+
+	private readonly PrintErrorFix = (text: string, error: string, file: string) => {
+		const start = file.indexOf(`${text}`);
 		const end = file.indexOf(`${error}`) + `${error}`.length;
 		const err = file.slice(start, end);
-        const e = file.replace(err, Colors.bgBrightMagenta + err + Colors.reset).split("\r\n");
+		const e = file
+			.replace(err, Colors.bgBrightMagenta + err + Colors.reset)
+			.split("\r\n");
 
-		this._logger.execute("Линия с ошибкой выделена светлой маджентой", Colors.bgBrightMagenta);
+		this._logger.execute(
+			"Линия с ошибкой выделена светлой маджентой",
+			Colors.bgBrightMagenta
+		);
 
-        console.log();
+		console.log();
 		this._logger.execute("See your file:");
-		console.log(e.map((l, i) => `${Number(i)+1}. ${l}`).join("\r\n"));
-        console.log();
+		console.log(e.map((l, i) => `${Number(i) + 1}. ${l}`).join("\r\n"));
+		console.log();
 
-		this._logger.execute("Линия с ошибкой выделена светлой маджентой", Colors.bgBrightMagenta);
-    }
+		this._logger.execute(
+			"Линия с ошибкой выделена светлой маджентой",
+			Colors.bgBrightMagenta
+		);
+	};
 
-    private readonly LineValidator = (file: string, lines: string[]): boolean => {
-        for(const index in lines) {
-            const line = lines[index];
-            const lineNumber = Number(index) + 1;
+	private readonly LineValidator = (file: string, lines: string[]): boolean => {
+		for (const index in lines) {
+			const line = lines[index];
+			const lineNumber = Number(index) + 1;
 
-            if (!line.includes(";")) {
-                this._logger.execute(`Линия ${lineNumber} не имеет ";"`, Colors.red);
-                this.PrintErrorFix(line, line, file);
+			if (!line.includes(";")) {
+				this._logger.execute(`Линия ${lineNumber} не имеет ";"`, Colors.red);
+				this.PrintErrorFix(line, line, file);
 
-                return false;
-            }
-        };
+				return false;
+			}
+		}
 
-        return true;
-    };
+		return true;
+	};
 
-    private readonly Validator = (file: string, lines: string[]): boolean => {
-        if (!this.LineValidator(file, lines))
-            return false;
+	private readonly Validator = (file: string, lines: string[]): boolean => {
+		if (!this.LineValidator(file, lines)) return false;
 
-        return true;
-    };
+		return true;
+	};
 
-    public init(fl?: string) {
-        const [ file, lines, isOk ] = this.data(fl);
+	public init(fl?: string) {
+		const [file, lines, isOk] = this.data(fl);
 
-        if (!isOk) return false;
+		if (!isOk) return false;
 
-        return this.Validator(file, lines);
-    };
+		return this.Validator(file, lines);
+	}
 
-    get logger(): Logger<"Validator"> {
-        return this._logger;
-    }
-};
+	get logger(): Logger<"Validator"> {
+		return this._logger;
+	}
+}
 
 export default Validator;
